841abd6d853765a0d1a0addccd8658181f7a87f0
PrithuKilledCode <psrivastava@innow8apps.com>
2024-04-12 17:02:33 +0530

    first commit

---
+++ src/controller/user.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import prisma from "../DB/db.config.js";
+import { executionAsyncResource } from "async_hooks";
+export const createUser = async(req:Request,res:Response) =>{
+  const {username,password,email}=req.body;
+  const findUser=await prisma.user.findUnique({
+    where:{
+      email:email
+    }
+  })
+  if(findUser){
+    return res.status(400).json({message:"Email already exists"})
+  }
+  const newUser=await prisma.user.create({
+    data:{
+      username:username,
+      password:password,
+      email:email
+    }
+  })
+  return res.status(200).json({message:"User created successfully",status:200,data:newUser})
+}
